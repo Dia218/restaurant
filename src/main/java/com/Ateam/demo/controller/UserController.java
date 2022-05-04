d01187883c47506a8fefa6548b53fe048970db6b
KYUNGWON JUNG <103038606+kw99j10@users.noreply.github.com>
2022-05-04 22:50:20 +0900

    로그인&회원가입

---
+++ src/main/java/com/Ateam/demo/controller/UserController.java
@@ -0,0 +1,106 @@
+package com.Ateam.demo.controller;
+
+import com.Ateam.demo.service.UserService;
+import com.Ateam.demo.vo.UserVo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    /**
+     * localhost:8080 시 index(메인화면) 으로 redirect //예정
+     * @return
+     */
+    @GetMapping
+    public String root() {
+        return "redirect:/login";
+    }
+
+    @GetMapping({"/login"})
+    public String login() {
+        return "login";
+    }
+
+    /**
+     * 회원가입 폼
+     * @return
+     */
+    @GetMapping({"/signUp"})
+    public String signUpForm() {
+        return "signup";
+    }
+
+    /**
+     * 로그인 실패 폼
+     * @return
+     */
+    @GetMapping({"/login_failed"})
+    public String loginFailed() {
+        return "login_failed";
+    }
+
+    /**
+     * 페이지 접근 실패 (권한)
+     * @return
+     */
+    @GetMapping({"/access_denied"})
+    public String access_Denied() {
+        return "access_denied";
+    }
+
+    /**
+     * 회원가입 진행
+     * @param userVo
+     * @return
+     */
+
+    @PostMapping({"/signUp"})
+    public String signUp(UserVo userVo) {
+        this.userService.joinUser(userVo);
+        return "redirect:/login";
+    }
+
+    /**
+     * 유저 페이지
+     * @param model
+     * @param authentication
+     * @return
+     */
+
+    @GetMapping({"/guest"})
+    public String userAccess(Model model, Authentication authentication) {
+        //Authentication 객체를 통해 유저 정보를 가져올 수 있다.
+        //UserVo userVo = (UserVo)authentication.getPrincipal(); //userDetail 객체를 가져옴
+        //model.addAttribute("info", String.format("%s의 %s님 환영합니다.%s님의 전화번호: %s", userVo.getUserId(), userVo.getUserName(), userVo.getUserId(), userVo.getUserPhone()));
+        return "guest";
+    }
+
+    @GetMapping({"/manager"})
+    public String managerAccess() {
+        //Authentication 객체를 통해 유저 정보를 가져올 수 있다.
+        //UserVo userVo = (UserVo) authentication.getPrincipal();  //userDetail 객체를 가져옴
+        //model.addAttribute("info", userVo.getUserId() +"의 "+ userVo.getUserName()+ "님");      //유저 아이디
+        return "manager/manager";
+    }
+
+    //유저용 예약 화면 매핑
+    @GetMapping({"/createReservationGuest"})
+    public String CreateReservationGuest(Model model, Authentication authentication) {
+
+        return "createReservationGuest";
+    }
+
+    //직원용 예약 화면 매핑
+    @GetMapping({"/createReservationAdmin"})
+    public String CreateReservationAdmin(Model model,Authentication authentication) {
+
+        return "manager/createReservationAdmin";
+    }
+}
---
+++ src/main/java/com/Ateam/demo/service/UserService.java
@@ -0,0 +1,48 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package com.Ateam.demo.service;
+
+import com.Ateam.demo.mapper.UserMapper;
+import com.Ateam.demo.vo.UserVo;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:sss");
+    Date time = new Date();
+    String localTime = format.format(time);
+    private final UserMapper userMapper;
+
+    @Transactional
+    public void joinUser(UserVo userVo) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        userVo.setUserPw(passwordEncoder.encode(userVo.getPassword()));
+        userVo.setUserAuth("ROLE_USER");  // ROLE_ADMIN 계정은 일단 디비 테이블상에서 수동으로 UPDATE
+        userVo.setUserPhone(userVo.getUserPhone());
+        userVo.setAppendDate(this.localTime);
+        userVo.setUpdateDate(this.localTime);
+        this.userMapper.saveUser(userVo);
+    }
+
+    public UserVo loadUserByUsername(String userId) throws UsernameNotFoundException {
+        //여기서 받은 유저 패스워드와 비교하여 로그인 인증
+        UserVo userVo = this.userMapper.getUserAccount(userId);
+        if (userVo == null) {
+            throw new UsernameNotFoundException("User not authorized.");
+        } else {
+            return userVo;
+        }
+    }
+}
