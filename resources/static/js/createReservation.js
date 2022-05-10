550ac94b4c49f3b11dfa691b0602b64ed9fffebd
KYUNGWON JUNG <103038606+kw99j10@users.noreply.github.com>
2022-05-10 20:37:30 +0900

    Update createReservation.js

--- resources/static/js/createReservation.js
+++ resources/static/js/createReservation.js
@@ -5,7 +5,7 @@
     */
 //
 // Scripts
-// 
+//
 
 window.addEventListener('DOMContentLoaded', event => {
 
@@ -33,11 +33,11 @@ function table_click(num) {
 
 //예약 생성 유효성 검사
 function CheckReservationUserForm() {
-    var TableNumber = document.getElementById("selected-table-number");
-    var selectedSits = document.getElementById("selected-sits");
-    var startDate = document.getElementById("startDate");
-    var startTime = document.getElementById("startTime");
-    var CustomerID =  document.getElementById("CustomerID");
+    var tableNo = document.getElementById("selected-table-number");
+    var tableSits = document.getElementById("selected-sits");
+    var reservationDate = document.getElementById("startDate");
+    var reservationTime = document.getElementById("startTime");
+    var userId =  document.getElementById("userId");
     var now = new Date();
     var nYY = now.getFullYear();
     var nMO = ('0' + (now.getMonth() + 1)).slice(-2);
@@ -45,49 +45,49 @@ function CheckReservationUserForm() {
     var nHH = now.getHours();
     var nMM = now.getMinutes();
 
-    var replaceDate = startDate.value.replace(/\-/g, "");
+    var replaceDate = reservationDate.value.replace(/-/g, "");
     var sYY = replaceDate.substring(0, 4);
     var sMO = replaceDate.substring(4, 6);
     var sDD = replaceDate.substring(6, 8);
 
-    var replaceTime = startTime.value.replace(/\:/g, "");
+    var replaceTime = reservationTime.value.replace(/:/g, "");
     var sHH = replaceTime.substring(0, 2);
     var sMM = replaceTime.substring(2, 4);
 
-    if (TableNumber.value == "") {
+    if (tableNo.value === "") {
         alert("테이블을 정해주세요.");
-        TableNumber.select();
-        TableNumber.focus();
+        tableNo.select();
+        tableNo.focus();
         return false;
-    } else if (selectedSits.value == "") {
+    } else if (tableSits.value === "") {
         alert("자릿 수를 선택해주세요.");
-        selectedSits.select();
-        selectedSits.focus();
+        tableSits.select();
+        tableSits.focus();
         return false;
-    } else if (startDate.value == "") {
+    } else if (reservationDate.value === "") {
         alert("날짜를 선택해주세요.");
-        startDate.select();
-        startDate.focus();
+        reservationDate.select();
+        reservationDate.focus();
         return false;
-    } else if (startTime.value == "") {
+    } else if (reservationTime.value === "") {
         alert("시간을 선택해주세요.");
-        startTime.select();
-        startTime.focus();
+        reservationTime.select();
+        reservationTime.focus();
         return false;
-    } else if (sYY == nYY && sMO == nMO && sDD == nDD && sHH - nHH < 0) {
+    } else if (sYY === nYY && sMO === nMO && sDD === nDD && sHH - nHH < 0) {
         alert("금일을 선택하셨습니다. 현재 시간 이후를 선택해주세요.");
-        startTime.select();
-        startTime.focus();
+        reservationTime.select();
+        reservationTime.focus();
         return false;
-    } else if (sYY == nYY && sMO == nMO && sDD == nDD && sHH == nHH && sMM - nMM < 0) {
+    } else if (sYY === nYY && sMO === nMO && sDD === nDD && sHH === nHH && sMM - nMM < 0) {
         alert("금일을 선택하셨습니다. 현재 시간 이후를 선택해주세요.");
-        startTime.select();
-        startTime.focus();
+        reservationTime.select();
+        reservationTime.focus();
         return false;
-    } else if(CustomerID.value == "") {
+    } else if(userId.value === "") {
         alert("고객님의 ID를 적어주세요.");
-        CustomerID.select();
-        CustomerID.focus();
+        userId.select();
+        userId.focus();
         return false;
     }
     return true;
