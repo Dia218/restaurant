142b69f01abcff771bedd0cfbb5774e5da99fba4
KYUNGWON JUNG <103038606+kw99j10@users.noreply.github.com>
2022-05-14 15:36:59 +0900

    직원 예약 등록 구현

---
+++ demo/configuration/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.Ateam.demo.configuration;
+
+import com.Ateam.demo.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+@EnableGlobalAuthentication
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserService userService;
+
+    /**
+     * 규칙 설정
+     * @param http
+     * @throws Exception
+     */
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers( "/index","/login", "/singUp", "/enroll","/check","/login_failed", "/resources/**").permitAll() // 로그인 권한은 누구나, resources파일도 모든권한
+                // USER, ADMIN 접근 허용 설정
+                .antMatchers("/guest").hasAnyRole("USER","ADMIN") //모두 가능
+                .antMatchers("/manager").hasRole("ADMIN") //관리자만 가능
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/login_proc")
+                //.defaultSuccessUrl("/guest")
+                .defaultSuccessUrl("/manager")
+                .failureUrl("/login_failed") // 인증에 실패했을 때 보여주는 화면 url, 로그인 form으로 파라미터값 error=true로 보낸다.
+                .and()
+                .exceptionHandling()
+                .accessDeniedPage("/access_denied")
+                .and()
+                .csrf().disable();		//로그인 창
+    }
+
+    /**
+     * 로그인 인증 처리 메소드
+     * @param auth
+     * @throws Exception
+     */
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(this.userService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+}
